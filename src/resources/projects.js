df65fbd25b216980199612d7e455e2899b0d27bc
shardul <maidkarshadow@gmail.com>
2025-04-18 15:18:59 +0530

    Your commit message here

---
+++ src/resources/projects.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+const projectsData = [
+    {
+      title: "serenity yoga landing page",
+      image: "/src/assets/yoga.png",
+      
+      link: "https://github.com/SHADOW712345678910/project-2",
+    }
+    ,
+    {
+      title: "Spotify Clone",
+      image: "/src/assets/clone.png",
+
+      link: "https://github.com/SHADOW712345678910/project_1",
+    }
+    ,
+    {
+      title: "food delivery website",
+      image: "/src/assets/fooddelivery.png",
+      
+      link: "https://github.com/SHADOW712345678910/project_3",
+    }
+    ,
+    {
+      title: "Ecommerce website",
+      image: "/src/assets/Screenshot 2025-04-17 161508.png",
+      
+      link: "https://github.com/SHADOW712345678910/ecommerce-website",
+    }
+
+
+]
+
+
+export default projectsData;
